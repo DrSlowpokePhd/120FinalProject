20d7c3301b494800c93c623ff4f49e9b79bab776
James Doherty <dohertyjames1999@gmail.com>
2021-05-14 14:46:06 -0700

    implemented a basic dialogue box class which can be used by NPCs

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -18,14 +18,41 @@ class Play extends Phaser.Scene {
         keyRIGHT = this.input.keyboard.addKey('RIGHT');
         keyR = this.input.keyboard.addKey('R');
         keySPACE = this.input.keyboard.addKey('SPACE');
+        keyD = this.input.keyboard.addKey('D');
+        // reserve dialog box 
+        let dialogStyle = {
+                backgroundColor: '#5d5861',
+                align: 'left',
+                maxLines: '6',
+                fontSize: '16px',
+                fixedHeight: 100,
+                fixedWidth: 175,
+                wordWrap: { width: 260 }
+            }
+        
+        let dialogBox = new DialogueBox (this, 50, 150, "This is a test.", dialogStyle);
+        dialogBox.visible = false;
+        // on key d down
+        keyD.on('down', function(event) { 
+            if (dialogBox.visible == false) {
+                dialogBox.visible = true;
+            } else {
+                dialogBox.visible = false;
+            }
+        });
     } 
 
     update() {
         this.player.update();
+        // restart scene
         if (keyR.isDown) {
             this.scene.restart();
         }
-
+        
+        // press d to open text dialog box
+        if (keyD.isDown) {
+            
+        }
         this.physics.add.collider(this.player, this.ground);
     }
 }
