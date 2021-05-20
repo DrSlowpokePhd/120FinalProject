b24475a8a14b9c7543c924e8cfc60e3816f509c5
Nanxiang Wang <nwang22@ucsc.edu>
2021-05-20 15:12:33 -0700

    updated tileset

---
+++ Assets/TowerTiles.tsx
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Tower Game" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="backgroundTiles.png" trans="000000" width="512" height="512"/>
+ <tile id="4" type="ToBeCleaned">
+  <properties>
+   <property name="objectType" value="cobweb"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="12" type="ToBeCleaned">
+  <properties>
+   <property name="objectType" value="trash"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="27.2727" y="17.4545">
+    <polygon points="0,0 -2.90909,2.72727 -5.09091,7.81818 -7.63636,11.0909 -8.54545,15.2727 -11.4545,18.5455 -13.8182,21.0909 -16.9091,26.9091 -20.7273,34 -22.3636,37.2727 -22.7273,42.7273 -22.9091,45.2727 -21.6364,46.1818 28.5455,45.8182 31.6364,37.0909 32.1818,32.3636 30.5455,29.0909 26.5455,26 23.2727,22.7273 20.9091,18.3636 16.3636,14.9091 11.2727,7.09091 10.5455,1.45455 5.81818,-0.909091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="3">
+   <object id="9" x="0" y="0" width="64" height="64"/>
+   <object id="10" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="48">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="52">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="63"/>
+  </objectgroup>
+ </tile>
+</tileset>
