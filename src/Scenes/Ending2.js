f6224a2079bdb848fed18f68b73dce4e48791ac5
Johnny Choi <cchoi14@ucsc.edu>
2021-06-08 02:02:35 +0900

    added the other two ending files

---
+++ src/Scenes/Ending2.js
@@ -0,0 +1,52 @@
+class Ending2 extends Phaser.Scene  {
+    constructor() {
+       super("ending2"); 
+    }
+    // this is ending card 2
+
+    preload() {
+        // load background asset
+        this.load.image('ending2_background', './Assets/Backgrounds/newspaper2.png');
+    }
+
+    create() {
+        // add key inputs
+        keySPACE = this.input.keyboard.addKey('SPACE');
+
+        // add backgrounds
+        this.background = this.add.image(0, 0, 'ending2_background').setOrigin(0, 0);
+
+        // create text
+        this.theEnd = this.add.text(1075, 37, "The End.", {
+            fontFamily: 'Eight Bit Dragon',
+            fontSize: '32px',
+            align: 'center'
+        });
+        this.tryAgain = this.add.text(1057, 86, "Try again for\na different\nending!", {
+            fontFamily: 'Eight Bit Dragon',
+            fontSize: '24px',
+            align: 'center'
+        });
+        this.continue = this.add.text(1035, 630, "Press SPACE\nto continue.", {
+            fontFamily: 'Eight Bit Dragon',
+            fontSize: '32px',
+            align: 'center'
+        });
+
+        // add text animation
+        this.textTween = this.tweens.add({
+            targets: [ this.continue ],
+            alpha: 0.1,
+            duration: 2000,
+            yoyo: true,
+            repeat: -1
+        });
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            this.scene.start('credits');
+        }
+    }
+}
+
